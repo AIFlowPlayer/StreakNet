2f6ad97a67ae9b38787221415acc7999c8e5a6c5
Coder.AN <an.hongjun@foxmail.com>
2024-01-10 19:58:42 +0800

    update visualize dataset

--- scripts/visualize_dataset.py
+++ scripts/visualize_dataset.py
@@ -4,6 +4,7 @@
 # Email: an.hongjun@foxmail.com
 
 import os 
+import cv2
 import yaml 
 import argparse
 import numpy as np
@@ -33,22 +34,31 @@ def main(args):
     configs.append(["train", read_config(os.path.join(args.path, "train_config.yaml"))])
     configs.append(["valid", read_config(os.path.join(args.path, "valid_config.yaml"))])
     configs.append(["test", read_config(os.path.join(args.path, "test_config.yaml"))])
-    lens = [len(config["config"]) for _, config in configs]
+    lens = [len(config["sub_datasets"]) for _, config in configs]
     max_len = np.max(lens)
-    
+    plt.figure(figsize=(18, 8))
+    plt.suptitle("Visualize Dataset")
     for j, (name, config) in enumerate(configs):
         sub_datasets = []
+        area = [0 for _ in range(max_len)]
         for name_i in config["sub_datasets"]:
-            gd = np.load(os.path.join(args.path, name_i, "groundtruth.npy"))
+            gd = np.load(os.path.join(args.path, name_i, "groundtruth.npy")) * 255
+            gd = cv2.cvtColor(gd, cv2.COLOR_GRAY2RGB)
             sub_datasets.append(gd)
         for i, info in enumerate(config["config"]):
-            plt.subplot(max_len, 3, 3 * i + j + 1)
-            plt.title("{}-{}".format(name, i + 1))
             idx = info["sub_idx"]
             col = info["col_slice"]
             row = info["row_slice"]
-            roi = sub_datasets[idx][row[0]:row[1], col[0]:col[1]]
-            plt.imshow(roi.T, cmap="gray")
+            w = col[1] - col[0]
+            h = row[1] - row[0]
+            area[idx] += w * h
+            sub_datasets[idx][row[0]:row[1], col[0]:col[1]] = 0.5 * sub_datasets[idx][row[0]:row[1], col[0]:col[1]] + 0.5 * np.array([[[255, 0, 0]]])
+        for i, name_i in enumerate(config["sub_datasets"]):
+            plt.subplot(max_len, 3, 3 * i + j + 1)
+            plt.title("{}-{}".format(name_i, name))
+            im = cv2.UMat(sub_datasets[i].transpose(1, 0, 2))
+            im = cv2.putText(im, "area:{}".format(area[i]), (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 2, color=(255, 255, 255), thickness=3)
+            plt.imshow(cv2.UMat.get(im), cmap="gray")
     plt.show()
 
 
